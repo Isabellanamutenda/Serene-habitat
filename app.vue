88ec863242a5ca0efab87a07d586de19a3962eb6
Isabellanamutenda <namtendaisabella@gmail.com>
2026-05-04 12:25:29 +0300

    fix sidebar

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-[#dce9e7] text-slate-800">
+  <div class="min-h-screen bg-[#dce9e7] text-slate-800 overflow-y-scroll">
     <!-- Page content -->
     <main class="p-6">
       <NuxtPage />
